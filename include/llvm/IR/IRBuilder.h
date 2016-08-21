62d2898f0a4806e91ae436cab3e2072d06f89126
Juneyoung Lee <aqjune@gmail.com>
2016-08-21 14:40:44 +0900

    Fix LoopUnswitch bug so that it correctly replaces all uses with the new freeze value Fix FoldSelectOpOp not to replace all uses when condition value is constant

--- include/llvm/IR/IRBuilder.h
+++ include/llvm/IR/IRBuilder.h
@@ -1778,7 +1778,9 @@ public:
   Value *CreateFreezeAtDef(Value *Arg, Function *F, const Twine &Name = "") {
     if (FreezeInst::isGuaranteedNotToBeUndef(Arg))
       return Arg;
-    
+
+    assert (!isa<Constant>(Arg) && "Constant has no def");
+
     if (Instruction *I = dyn_cast<Instruction>(Arg)) {
       FreezeInst *FI = new FreezeInst(I, Name);
       BasicBlock *BB = I->getParent();
@@ -1798,7 +1800,7 @@ public:
       FI->replaceUsesOfWith(FI, Arg);
       return FI;
     }
-    
+
     assert((isa<PHINode>(Arg) || isa<Constant>(Arg) || isa<Argument>(Arg)) &&
         "Cannot freeze the value");
     return nullptr;
